f53a7e5589d20aa09c9fbedbb16a4b278d3eba00
wensan <wensansiegfred@gmail.com>
2017-01-21 08:48:35 +0800

    Hospital Management

---
+++ ohospital/src/main/java/com/ondoymatics/OhospitalApplication.java
@@ -0,0 +1,12 @@
+package com.ondoymatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhospitalApplication.class, args);
+	}
+}
